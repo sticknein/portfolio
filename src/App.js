4742e0b56c2cec0b09f92ac86e4aeb9fba4df770
sticknein <sticknein@gmail.com>
2022-05-10 20:02:06 -0700

    changed projects to listener

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
 import './App.css';
 import prof_pic from './images/prof_pic.jpg';
+import spotify_logo from './images/spotify-logo.png';
 import cranium_tile from './images/cranium.png';
+import roland_logo from './images/roland_logo.png';
+import weather_logo from './images/weather_logo.jpeg';
+import wiki_logo from './images/wiki_logo.png';
 
 function App({ active }) {
   return (
     <div className='App'>
+      <div id='col-1'>
+        <a href='https://holophonemusic.com/' target='_blank'>
         <img 
             className='prof-pic'
             src={prof_pic} 
             alt='Profile Picture' 
         />
-      <div className='about-me'>
+      </a>
+      </div>
+      <div id='col-2'>
+        <div className='about-me'>
             <h1 id='salutation'>Hi, I'm Nick Stein</h1>
             <h3 id='subtitle'>Full-Stack Web Developer</h3>
             <p id='bio'>
-              I’m a former professional musician who discovered a love of coding during the pandemic and decided to pursue a career in tech. 
-              I posses a BA in Business and have experience delivering results from small teams with limited resources in a variety of environments. 
-              I’m looking for an opportunity to join a tech company and apply my relevant business knowledge, operations experience, and growing technical expertise.
+              I’m a former professional musician who discovered a love of coding during the pandemic.  Let's build things!
             </p>
-            <h4>Explore some of my projects:</h4>
+            <h4>Explore my current project:</h4>
             <div className='projects'>
-              <div className='project-column'>
-                <div className={`project ${active && 'project--active'}`} id='cranium'>
-                  <img className='app-tile' alt='Cranium App' src={cranium_tile} />
-                  <figcaption>Cranium | Ticketing App</figcaption>
-                </div>
-                <div className='project' id='stick909'>
-                  <img className='app-tile' alt='Cranium App' src={cranium_tile} />
-                  <figcaption>Cranium | Ticketing App</figcaption>
-                </div>
-              </div>
-              <div className='project-column'>
-                <div className='project' id='weather'>
-                <img className='app-tile' alt='Cranium App' src={cranium_tile} />
-                <figcaption>Cranium | Ticketing App</figcaption>
-              </div>
-              <div className='project' id='wiki'>
-                <img className='app-tile' alt='Cranium App' src={cranium_tile} />
-                <figcaption>Cranium | Ticketing App</figcaption>
-              </div>
+              <div className='project'>
+                <a
+                id='listener'
+                href='https://sticknein-listener.herokuapp.com'
+                target='_blank' 
+              >
+                <img className='app-tile' alt='Listener App' src={spotify_logo} />
+                <figcaption>listener - share your listening activity with friends</figcaption>
+              </a>
               </div>
             </div>
-      </div>    
+            <div className='contact-container'>
+              <a 
+              href='mailto:sticknein@gmail.com'
+              className='contact-button-small'
+            >
+              Email Me
+            </a>
+            </div>
+        </div>   
+      </div>
+      <div id='col-3'>
+        <a 
+        href='mailto:sticknein@gmail.com'
+        className='contact-button'
+      >
+        Email Me
+      </a>
     </div>
+  </div>
   );
 }
 
